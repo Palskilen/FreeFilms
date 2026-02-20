c813bfe3dcd3f69ba4adaed87f6c9a4631c2ed24
Palskilen <palfanart@gmail.com>
2026-02-20 14:30:43 +0100

    Add files via upload

--- movies.js
+++ movies.js
@@ -93,6 +93,269 @@ window.movieData = {
             }
         }
     },
+    "MARVEL" : {
+        "Kapitan Ameryka: Pierwsze starcie" : {
+            "type" : "Film",
+            "name": "Kapitan Ameryka: Pierwsze starcie",
+            "year": "2011",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "124m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Kapitan Marvel" : {
+            "type" : "Film",
+            "name": "Kapitan Marvel",
+            "year": "2019",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "124m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Iron Man" : {
+            "type" : "Film",
+            "name": "Iron Man",
+            "year": "2008",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "126m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Iron Man 2" : {
+            "type" : "Film",
+            "name": "Iron Man 2",
+            "year": "2010",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "124m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Niesamowity Hulk" : {
+            "type" : "Film",
+            "name": "Niesamowity Hulk",
+            "year": "2008",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "114m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Thor" : {
+            "type" : "Film",
+            "name": "Thor",
+            "year": "2011",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "115m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Avengers" : {
+            "type" : "Film",
+            "name": "Avengers",
+            "year": "2012",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "143m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Ant-Man" : {
+            "type" : "Film",
+            "name": "Ant-Man",
+            "year": "2015",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "117m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Kapitan Ameryka: Wojna bohaterów" : {
+            "type" : "Film",
+            "name": "Kapitan Ameryka: Wojna bohaterów",
+            "year": "2016",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "147m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Czarna Wdowa" : {
+            "type" : "Film",
+            "name": "Czarna Wdowa",
+            "year": "2021",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "134m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Czarna Pantera" : {
+            "type" : "Film",
+            "name": "Czarna Pantera",
+            "year": "2018",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "134m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Spider-Man: Homecoming" : {
+            "type" : "Film",
+            "name": "Spider-Man: Homecoming",
+            "year": "2017",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "133m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Doktor Strange" : {
+            "type" : "Film",
+            "name": "Doktor Strange",
+            "year": "2016",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "115m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Thor: Ragnarok" : {
+            "type" : "Film",
+            "name": "Thor: Ragnarok",
+            "year": "2017",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "130m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Ant-Man i Osa" : {
+            "type" : "Film",
+            "name": "Ant-Man i Osa",
+            "year": "2018",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "118m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Avengers: Wojna bez granic" : {
+            "type" : "Film",
+            "name": "Avengers: Wojna bez granic",
+            "year": "2018",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "149m",
+            "screen" : "https://bajeczki24.pl/images/v1/format=auto,quality=75,fit=cover,scq=50,w=300,h=450/avengers-wojna-bez-granic-poster.webp",
+            "URL": "https://bysesukior.com/e/bq06bkm0zc3j"
+        },
+        "Avengers: Koniec gry" : {
+            "type" : "Film",
+            "name": "Avengers: Koniec gry",
+            "year": "2019",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "181m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Shang-Chi i legenda dziesięciu pierścieni" : {
+            "type" : "Film",
+            "name": "Shang-Chi i legenda dziesięciu pierścieni",
+            "year": "2021",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "132m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Spider-Man: Daleko od domu" : {
+            "type" : "Film",
+            "name": "Spider-Man: Daleko od domu",
+            "year": "2019",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "129m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Eternals" : {
+            "type" : "Film",
+            "name": "Eternals",
+            "year": "2021",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "156m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Spider-Man: Bez drogi do domu" : {
+            "type" : "Film",
+            "name": "Spider-Man: Bez drogi do domu",
+            "year": "2021",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "148m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Deadpool i Wolverine" : {
+            "type" : "Film",
+            "name": "Deadpool i Wolverine",
+            "year": "TBA",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "TBA",
+            "screen" : "",
+            "URL": ""
+        },
+        "Doktor Strange w multiwersum obłędu" : {
+            "type" : "Film",
+            "name": "Doktor Strange w multiwersum obłędu",
+            "year": "2022",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "126m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Wakanda Forever" : {
+            "type" : "Film",
+            "name": "Wakanda Forever",
+            "year": "2022",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "161m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Thor: Miłość i grom" : {
+            "type" : "Film",
+            "name": "Thor: Miłość i grom",
+            "year": "2022",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "130m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Ant-Man i Osa: Kwantomania" : {
+            "type" : "Film",
+            "name": "Ant-Man i Osa: Kwantomania",
+            "year": "2023",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "125m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Strażnicy Galaktyki Vol. 3" : {
+            "type" : "Film",
+            "name": "Strażnicy Galaktyki Vol. 3",
+            "year": "2023",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "150m",
+            "screen" : "",
+            "URL": ""
+        },
+        "Marvels" : {
+            "type" : "Film",
+            "name": "Marvels",
+            "year": "2023",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "TBA",
+            "screen" : "",
+            "URL": ""
+        },
+        "Kapitan Ameryka: Nowy wspaniały świat" : {
+            "type" : "Film",
+            "name": "Kapitan Ameryka: Nowy wspaniały świat",
+            "year": "2024",
+            "genre": "Akcja, Sci-Fi, Przygodowy",
+            "duration": "TBA",
+            "screen" : "",
+            "URL": ""
+        },
+    },
     "WARNES BROS" : {
         "Harry Potter i Kamień Filozoficzny" : {
             "type" : "Film",
@@ -108,7 +371,7 @@ window.movieData = {
             "name": "Harry Potter i Komnata Tajemnic",
             "year": 2002,
             "genre": "Fantasy Przygodowy",
-            "duration": "152m",
+            "duration": "161m",
             "screen" : "https://bajeczki24.pl/images/v1/format=auto,quality=75,fit=cover,scq=50,w=300,h=450/harry-potter-i-komnata-tajemnic-poster.webp",
             "URL": "https://bysesukior.com/e/cl23vscsvwtp"
         },
@@ -117,7 +380,7 @@ window.movieData = {
             "name": "Harry Potter i więzień Azkabanu",
             "year": 2004,
             "genre": "Fantasy Przygodowy",
-            "duration": "152m",
+            "duration": "142m",
             "screen" : "https://bajeczki24.pl/images/v1/format=auto,quality=75,fit=cover,scq=50,w=300,h=450/harry-potter-i-wiezien-azkabanu-poster.webp",
             "URL": "https://bysesukior.com/e/ture8isgrbdd"
         },
@@ -126,7 +389,7 @@ window.movieData = {
             "name": "Harry Potter i Czara Ognia",
             "year": 2005,
             "genre": "Fantasy Przygodowy",
-            "duration": "152m",
+            "duration": "157m",
             "screen" : "https://bajeczki24.pl/images/v1/format=auto,quality=75,fit=cover,scq=50,w=300,h=450/harry-potter-i-czara-ognia-poster.webp",
             "URL": "https://bysesukior.com/e/32x1h4ej3k6q"
         },
@@ -135,7 +398,7 @@ window.movieData = {
             "name": "Harry Potter i Zakon Feniksa",
             "year": 2007,
             "genre": "Fantasy Przygodowy",
-            "duration": "152m",
+            "duration": "138m",
             "screen" : "https://bajeczki24.pl/images/v1/format=auto,quality=75,fit=cover,scq=50,w=300,h=450/harry-potter-i-zakon-feniksa-poster.webp",
             "URL": "https://bysesukior.com/e/46uss340ajna"
         },
@@ -144,7 +407,7 @@ window.movieData = {
             "name": "Harry Potter i Książę Półkrwi",
             "year": 2009,
             "genre": "Fantasy Przygodowy",
-            "duration": "152m",
+            "duration": "153m",
             "screen" : "https://bajeczki24.pl/images/v1/format=auto,quality=75,fit=cover,scq=50,w=300,h=450/harry-potter-i-ksiaze-polkrwi-poster.webp",
             "URL": "https://bysesukior.com/e/d11uaujl4h97"
         },
@@ -153,7 +416,7 @@ window.movieData = {
             "name": "Harry Potter i Insygnia Śmierci: Część I",
             "year": 2001,
             "genre": "Fantasy Przygodowy",
-            "duration": "152m",
+            "duration": "146m",
             "screen" : "https://bajeczki24.pl/images/v1/format=auto,quality=75,fit=cover,scq=50,w=300,h=450/harry-potter-i-insygnia-smierci-czesc-i-poster.webp",
             "URL": "https://bysesukior.com/e/7wxe8fxst12s"
         },
@@ -162,7 +425,7 @@ window.movieData = {
             "name": "Harry Potter i Insygnia Śmierci: Część II",
             "year": 2011,
             "genre": "Fantasy Przygodowy",
-            "duration": "152m",
+            "duration": "130",
             "screen" : "https://bajeczki24.pl/images/v1/format=auto,quality=75,fit=cover,scq=50,w=300,h=450/harry-potter-i-insygnia-smierci-czesc-ii-poster.webp",
             "URL": "https://bysesukior.com/e/h30cazv7aj51"
         },
